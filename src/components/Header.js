a1825f8230ba2aa038d8cb0e0a194a9379814ac9
Celeste Zapata <zdceleste@gmail.com>
2024-09-08 21:19:10 -0300

    Add semantic HTML components / Add Open Graph meta tags

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../images/little-lemon-logo.webp'
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon Logo" />
+      <h1>Little Lemon</h1>
+    </header>
+  )
+}
+
+export default Header
